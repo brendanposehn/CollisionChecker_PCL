27200065088be65557d9f6e5c9e73d6eafdfa5d4
brendanposehn <brendanposehn0@gmail.com>
2020-11-03 22:22:43 -0800

    Able to load in .ply file but getting error when initing pcl visualizer. Think I need to rebuild plc with different cpp standard.

--- CollisionChecker.cpp
+++ CollisionChecker.cpp
@@ -1,39 +1,41 @@
 #include <iostream>
+#include <boost/thread.hpp>
+#include <boost/shared_ptr.hpp>
 #include <pcl/common/time.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/point_types.h>
 #include <igl/opengl/glfw/Viewer.h>
+#include <string>
 
 int main(){
+
+    // we are using cpp 11 standards
+
     pcl::ScopeTime scope_time ("Test loop");
-    std::cout << "Done." << std::endl;
 
- // Inline mesh of a cube
-  const Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
-    0.0,0.0,0.0,
-    0.0,0.0,1.0,
-    0.0,1.0,0.0,
-    0.0,1.0,1.0,
-    1.0,0.0,0.0,
-    1.0,0.0,1.0,
-    1.0,1.0,0.0,
-    1.0,1.0,1.0).finished();
-  const Eigen::MatrixXi F = (Eigen::MatrixXi(12,3)<<
-    1,7,5,
-    1,3,7,
-    1,4,3,
-    1,2,4,
-    3,8,7,
-    3,4,8,
-    5,7,8,
-    5,8,6,
-    1,5,6,
-    1,6,2,
-    2,6,8,
-    2,8,4).finished().array()-1;
+    std::string meshfile ("/home/brend/BCCancer/meshes/boy/fromWindows/merged_mesh.ply");
+    pcl::PolygonMesh::Ptr polymesh (new pcl::PolygonMesh);
+    //what does the '... polymesh = (new pcl::PolygonMesh)' mean ?
+    //is it the same as '... polymesh = new pcl::PolygonMesh'?
+    pcl::PLYReader Reader;
+    Reader.read(meshfile, *polymesh);
 
-  // Plot the mesh
-  igl::opengl::glfw::Viewer viewer;
-  viewer.data().set_mesh(V, F);
-  viewer.data().set_face_based(true);
-  viewer.launch();
+    //boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));   
+    //getting error here: 'undefined reference to pcl::visualization::PCLVisualizer....
+    //might be due to building with wrong cpp source (apparently needs to be c++ 14 standards)
 
+    pcl::visualization::PCLVisualizer::Ptr viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+    //this fails too for same reason^^
+
+    // viewer->setBackgroundColor(0, 0, 0);
+    // viewer->addPolygonMesh(*polymesh,"meshes",0);
+    // viewer->addCoordinateSystem (1.0);
+    // viewer->initCameraParameters ();
+    // while (!viewer->wasStopped ()){
+    //     viewer->spinOnce (100);
+    //     boost::this_thread::sleep (boost::posix_time::microseconds (100000));
+    // }
+    //executes fine now 
 }
